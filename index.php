0e9f08a7e4b036129a21cdb7a7427ee4e46a1407
ataylor <ataylor@GMAP_RCB_2.grcs.local>
2013-04-16 16:02:22 -0400

    build menu

--- index.php
+++ index.php
@@ -5,16 +5,27 @@
         <title>Manifest Management</title>
         <link rel="stylesheet" href="rsc/manifest.css" type="text/css" />
         <link rel="shortcut icon" href="rsc/favicon.ico" />
+        <script>
+            function changepage(pagename){
+                var xmlhttp;
+                xmlhttp=new XMLHttpRequest();
+                xmlhttp.open("POST",pagename,false);
+                xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+                xmlhttp.send();
+                document.getElementById("content").innerHTML=xmlhttp.responseText;  
+                return false;
+            };
+         </script>
     </head>
     <body>
         <div id="banner">
             <a href="http://gmapserver/"><img src="rsc/ENC_Logo.png" alt="Encore!!!" width ="250" height="100"></a>
         </div>            
         <div id="menu">
-                <p>include something<br />
-                that links to<br />
-                other things.</p>
-            </div>
+            <a href="index.php" onclick="return changepage('mfst_entry.php')">Enter Manifest</a>
+            <a href="index.php" onclick="return changepage('mfst_receive.php')">Receive Manifest</a>
+            <a href="index.php" onclick="return changepage('mfst_edit.php')">Edit Manifest</a>
+        </div>
         <div id="main">
             <div id="title">
                 <h1>Manifest Management</h1>
@@ -24,8 +35,6 @@
             </div>
         </div>            
         <div id="sidebar">
-            <p>do useful things<br />
-            here.</p>
-        </div>
+        &nbsp;</div>
     </body>
 </html>
