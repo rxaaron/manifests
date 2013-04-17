38a468953a3698b6f7d15c5bfbb09bf66ad6739c
ataylor <github@rxtaylor.com>
2013-04-17 14:30:29 -0400

    edit page complete

--- mfst_edit.php
+++ mfst_edit.php
@@ -7,14 +7,33 @@ if (!$db) {
 } else {
     if (isset($_GET['ID'])){
         //specific manifest
-        $queryone=$db->query("SELECT A.ID, A.DateSent, B.Name AS Location, A.NumPages, C.Name AS Status, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID INNER JOIN Enc_MfstStatus AS C ON A.Status = C.ID WHERE ID=".$_GET['ID'].";");
+        $queryone=$db->query("SELECT A.ID, A.DateSent, B.Name AS Location, A.NumPages, A.Status, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID WHERE A.ID=".$_GET['ID'].";");
         if($queryone){
-            
+            $status=$db->query("SELECT ID, Name FROM Enc_MfstStatus WHERE Active=true;");
+            echo "<form name=\"mfst_edit\" action=\"scripts/mfst_update.php\" method=\"POST\" autocomplete=\"off\">";
+            echo "<table><colgroup><col name=\"label\" style=\"width:250px;\"><col name=\"boxes\" style=\"width:300px;\"></colgroup>";
+            while($resultone=$queryone->fetch_object()){
+                echo "<tr><td>Database ID:</td><td>".$resultone->ID."</td></tr>";
+                echo "<tr><td>Location:</td><td>".$resultone->Location."</td></tr>";
+                echo "<tr><td>Date Sent:</td><td>".date("m/d/Y",strtotime($resultone->DateSent))."</td></tr>";
+                echo "<tr><td>Number of Pages:</td><td>".$resultone->NumPages."</td></tr>";
+                echo "<tr><td>Status:</td><td><select id=\"status\">";
+                while($stats=$status->fetch_object()){
+                    if($resultone->Status==$stats->ID){
+                        echo "<option value=\"".$stats->ID."\" label=\"".$stats->Name."\" selected>".$stats->Name."</option>";
+                    }else{
+                        echo "<option value=\"".$stats->ID."\" label=\"".$stats->Name."\">".$stats->Name."</option>";
+                    }
+                }
+                echo "</select></td></tr>";
+                echo "<tr><td>Comments:</td><td><textarea id=\"comments\" rows=10 columns=30>".$resultone->Comments."</textarea></td></tr></table>";
+                echo "<input type=\"submit\" id=\"gobtn\" value=\"Update Manifest\" style=\"padding:10px 40px 10px 40px;\" /></form>";
+            }
         }
         
     }else{
         //general list
-        $queryall=$db->query("SELECT A.ID, A.DateSent, B.Name AS Location, A.NumPages, C.Name AS Status, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID INNER JOIN Enc_MfstStatus AS C ON A.Status = C.ID  ORDER BY A.DateSent DESC;");
+        $queryall=$db->query("SELECT A.ID, A.DateSent, B.Name AS Location, A.NumPages, C.Name AS Status, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID INNER JOIN Enc_MfstStatus AS C ON A.Status = C.ID  ORDER BY A.DateSent DESC, B.Name ASC;");
         if($queryall){
             echo "<table class=\"list\"><colgroup><col class=\"datesent\"><col class=\"location\"><col class=\"numpages\"><col class=\"status\"><col class=\"comments\"></colgroup><tr><th>Date Sent</th><th>Location</th><th>Pages</th><th>Status</th><th>Comments</th></tr>";
             while($resultall=$queryall->fetch_object()){
--- scripts/mfst_outstanding.php
+++ scripts/mfst_outstanding.php
@@ -7,7 +7,7 @@
         $query=$db->query("SELECT DISTINCT DateSent FROM Enc_Manifest WHERE Status = 1 OR Status = 3 ORDER BY DateSent DESC;");
         if ($query){
             while($result=$query->fetch_object()){
-                $subquery=$db->query("SELECT A.ID, B.Name, A.NumPages, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID WHERE A.DateSent=\"".$result->DateSent."\" AND (A.Status=1 OR A.Status=3);");
+                $subquery=$db->query("SELECT A.ID, B.Name, A.NumPages, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID WHERE A.DateSent=\"".$result->DateSent."\" AND (A.Status=1 OR A.Status=3) ORDER BY B.Name;");
                 if ($subquery){
                     echo "<h1>".date("m/d/Y",strtotime($result->DateSent))."</h1>";
                     while($subresult=$subquery->fetch_object()){
