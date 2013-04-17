0341830a1c021d2d152ff5a21626b547e106f6df
ataylor <github@rxtaylor.coml>
2013-04-17 13:49:05 -0400

    manifest edit step 1, showing full list

--- mfst_edit.php
+++ mfst_edit.php
@@ -7,15 +7,18 @@ if (!$db) {
 } else {
     if (isset($_GET['ID'])){
         //specific manifest
-        echo "I got pages!";
+        $queryone=$db->query("SELECT A.ID, A.DateSent, B.Name AS Location, A.NumPages, C.Name AS Status, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID INNER JOIN Enc_MfstStatus AS C ON A.Status = C.ID WHERE ID=".$_GET['ID'].";");
+        if($queryone){
+            echo "<table>";
+        }
         
     }else{
         //general list
-        $queryall=$db->query("SELECT A.ID, A.DateSent, B.Name, A.NumPages, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID ORDER BY A.DateSent DESC;");
+        $queryall=$db->query("SELECT A.ID, A.DateSent, B.Name AS Location, A.NumPages, C.Name AS Status, A.Comments FROM Enc_Manifest AS A INNER JOIN Enc_Locations AS B ON A.Location=B.ID INNER JOIN Enc_MfstStatus AS C ON A.Status = C.ID  ORDER BY A.DateSent DESC;");
         if($queryall){
-            echo "<table class=\"list\"><colgroup><col class=\"datesent\"><col class=\"location\"><col class=\"numpages\"><col class=\"comments\"></colgroup><tr><th>Date Sent</th><th>Location</th><th>Pages</th><th>Comments</th></tr>";
+            echo "<table class=\"list\"><colgroup><col class=\"datesent\"><col class=\"location\"><col class=\"numpages\"><col class=\"status\"><col class=\"comments\"></colgroup><tr><th>Date Sent</th><th>Location</th><th>Pages</th><th>Status</th><th>Comments</th></tr>";
             while($resultall=$queryall->fetch_object()){
-                echo "<tr><td>".$resultall->DateSent."</td><td><a href=\"index.php?ID=".$resultall->ID."\">".$resultall->Name."</a></td><td>".$resultall->NumPages."</td><td>".$resultall->Comments."</td></tr>";
+                echo "<tr><td>".$resultall->DateSent."</td><td><a href=\"index.php?ID=".$resultall->ID."\">".$resultall->Location."</a></td><td>".$resultall->NumPages."</td><td>".$resultall->Status."</td><td>".$resultall->Comments."</td></tr>";
             }
             echo "</table>";
         }
